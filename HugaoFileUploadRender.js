7fd13b4e1883da1fd60fd61010f0a188f0aec1b7
Hugo A. Cadena M <hugao1118@gmail.com>
2021-02-01 20:21:03 -0500

    remove endsWith function and start using genexus'

--- HugaoFileUploadRender.js
+++ HugaoFileUploadRender.js
@@ -1,12 +1,3 @@
-if (!String.prototype.endsWith) {
-	String.prototype.endsWith = function(search, this_len) {
-		if (this_len === undefined || this_len > this.length) {
-			this_len = this.length;
-		}
-		return this.substring(this_len - search.length, this_len) === search;
-	};
-}
-
 function HugaoFileUpload()
 {
 	this.UploadedFiles;
